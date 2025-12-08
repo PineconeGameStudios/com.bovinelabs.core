8c465574de96fee4bee00b76a4cafbcf1ced808a
Tim <6189147+tertle@users.noreply.github.com>
2025-12-08 18:31:55 +1100

    Added 4 mod trampoline. Fixed some documentation.

--- BovineLabs.Core/Utility/BurstTrampoline.cs
+++ BovineLabs.Core/Utility/BurstTrampoline.cs
@@ -19,6 +19,9 @@ namespace BovineLabs.Core.Utility
 
     internal delegate void BurstTrampolineInvoker3(IntPtr context, IntPtr data1Ptr, IntPtr data2Ptr, IntPtr data3Ptr);
 
+    internal delegate void BurstTrampolineInvoker4(IntPtr context, IntPtr data1Ptr, IntPtr data2Ptr, IntPtr data3Ptr, IntPtr data4Ptr);
+
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1649:File name should match first type name", Justification = "Design")]
     internal interface IBurstTrampolineInvoker0
     {
         void Invoke();
@@ -39,35 +42,9 @@ namespace BovineLabs.Core.Utility
         void Invoke(IntPtr data1Ptr, IntPtr data2Ptr, IntPtr data3Ptr);
     }
 
-    internal static class BurstTrampolineDispatcher
+    internal interface IBurstTrampolineInvoker4
     {
-        [AOT.MonoPInvokeCallback(typeof(BurstTrampolineInvoker0))]
-        internal static void Trampoline0(IntPtr context)
-        {
-            var handle = GCHandle.FromIntPtr(context);
-            ((IBurstTrampolineInvoker0)handle.Target!).Invoke();
-        }
-
-        [AOT.MonoPInvokeCallback(typeof(BurstTrampolineInvoker1))]
-        internal static void Trampoline1(IntPtr context, IntPtr data1Ptr)
-        {
-            var handle = GCHandle.FromIntPtr(context);
-            ((IBurstTrampolineInvoker1)handle.Target!).Invoke(data1Ptr);
-        }
-
-        [AOT.MonoPInvokeCallback(typeof(BurstTrampolineInvoker2))]
-        internal static void Trampoline2(IntPtr context, IntPtr data1Ptr, IntPtr data2Ptr)
-        {
-            var handle = GCHandle.FromIntPtr(context);
-            ((IBurstTrampolineInvoker2)handle.Target!).Invoke(data1Ptr, data2Ptr);
-        }
-
-        [AOT.MonoPInvokeCallback(typeof(BurstTrampolineInvoker3))]
-        internal static void Trampoline3(IntPtr context, IntPtr data1Ptr, IntPtr data2Ptr, IntPtr data3Ptr)
-        {
-            var handle = GCHandle.FromIntPtr(context);
-            ((IBurstTrampolineInvoker3)handle.Target!).Invoke(data1Ptr, data2Ptr, data3Ptr);
-        }
+        void Invoke(IntPtr data1Ptr, IntPtr data2Ptr, IntPtr data3Ptr, IntPtr data4Ptr);
     }
 
     public readonly struct BurstTrampoline
@@ -288,6 +265,69 @@ namespace BovineLabs.Core.Utility
         }
     }
 
+    public unsafe readonly struct BurstTrampoline<T1, T2, T3, T4>
+        where T1 : unmanaged
+        where T2 : unmanaged
+        where T3 : unmanaged
+        where T4 : unmanaged
+    {
+        private static readonly FunctionPointer<BurstTrampolineInvoker4> StaticInvoker = CreateInvokerFunctionPointer();
+
+        private readonly FunctionPointer<BurstTrampolineInvoker4> functionPointer;
+        private readonly IntPtr context;
+
+        public BurstTrampoline(Delegate method)
+        {
+            this.functionPointer = StaticInvoker;
+            var invoker = new TrampolineInvoker(method);
+            GarbagePrevention.Objects.Add(invoker);
+            var handle = GCHandle.Alloc(invoker);
+            this.context = GCHandle.ToIntPtr(handle);
+        }
+
+        public delegate void Delegate(in T1 data1, in T2 data2, in T3 data3, in T4 data4);
+
+        public bool IsCreated => this.functionPointer.IsCreated;
+
+        [SuppressMessage("StyleCop.CSharp.LayoutRules", "SA1519:Braces should not be omitted from multi-line child statement", Justification = "Fixed")]
+        public void Invoke(in T1 data1, in T2 data2, in T3 data3, in T4 data4)
+        {
+            fixed (T1* data1Ptr = &data1)
+            fixed (T2* data2Ptr = &data2)
+            fixed (T3* data3Ptr = &data3)
+            fixed (T4* data4Ptr = &data4)
+            {
+                this.functionPointer.Invoke(this.context, (IntPtr)data1Ptr, (IntPtr)data2Ptr, (IntPtr)data3Ptr, (IntPtr)data4Ptr);
+            }
+        }
+
+        private static FunctionPointer<BurstTrampolineInvoker4> CreateInvokerFunctionPointer()
+        {
+            BurstTrampolineInvoker4 invoker = BurstTrampolineDispatcher.Trampoline4;
+            GarbagePrevention.Objects.Add(invoker);
+            return new FunctionPointer<BurstTrampolineInvoker4>(Marshal.GetFunctionPointerForDelegate(invoker));
+        }
+
+        private sealed class TrampolineInvoker : IBurstTrampolineInvoker4
+        {
+            private readonly Delegate method;
+
+            public TrampolineInvoker(Delegate method)
+            {
+                this.method = method;
+            }
+
+            public void Invoke(IntPtr data1Ptr, IntPtr data2Ptr, IntPtr data3Ptr, IntPtr data4Ptr)
+            {
+                ref var value1 = ref UnsafeUtility.AsRef<T1>((void*)data1Ptr);
+                ref var value2 = ref UnsafeUtility.AsRef<T2>((void*)data2Ptr);
+                ref var value3 = ref UnsafeUtility.AsRef<T3>((void*)data3Ptr);
+                ref var value4 = ref UnsafeUtility.AsRef<T4>((void*)data4Ptr);
+                this.method(in value1, in value2, in value3, in value4);
+            }
+        }
+    }
+
     public unsafe readonly struct BurstTrampolineOut<T>
         where T : unmanaged
     {
@@ -464,6 +504,110 @@ namespace BovineLabs.Core.Utility
         }
     }
 
+    public unsafe readonly struct BurstTrampolineOut<T1, T2, T3, TOut>
+        where T1 : unmanaged
+        where T2 : unmanaged
+        where T3 : unmanaged
+        where TOut : unmanaged
+    {
+        private static readonly FunctionPointer<BurstTrampolineInvoker4> StaticInvoker = CreateInvokerFunctionPointer();
+
+        private readonly FunctionPointer<BurstTrampolineInvoker4> functionPointer;
+        private readonly IntPtr context;
+
+        public BurstTrampolineOut(Delegate method)
+        {
+            this.functionPointer = StaticInvoker;
+            var invoker = new TrampolineInvoker(method);
+            GarbagePrevention.Objects.Add(invoker);
+            var handle = GCHandle.Alloc(invoker);
+            this.context = GCHandle.ToIntPtr(handle);
+        }
+
+        public delegate void Delegate(in T1 data1, in T2 data2, in T3 data3, out TOut outData);
+
+        public bool IsCreated => this.functionPointer.IsCreated;
+
+        [SuppressMessage("StyleCop.CSharp.LayoutRules", "SA1519:Braces should not be omitted from multi-line child statement", Justification = "Fixed")]
+        public void Invoke(in T1 data1, in T2 data2, in T3 data3, out TOut outData)
+        {
+            outData = default;
+
+            fixed (T1* data1Ptr = &data1)
+            fixed (T2* data2Ptr = &data2)
+            fixed (T3* data3Ptr = &data3)
+            fixed (TOut* outDataPtr = &outData)
+            {
+                this.functionPointer.Invoke(this.context, (IntPtr)data1Ptr, (IntPtr)data2Ptr, (IntPtr)data3Ptr, (IntPtr)outDataPtr);
+            }
+        }
+
+        private static FunctionPointer<BurstTrampolineInvoker4> CreateInvokerFunctionPointer()
+        {
+            BurstTrampolineInvoker4 invoker = BurstTrampolineDispatcher.Trampoline4;
+            GarbagePrevention.Objects.Add(invoker);
+            return new FunctionPointer<BurstTrampolineInvoker4>(Marshal.GetFunctionPointerForDelegate(invoker));
+        }
+
+        private sealed class TrampolineInvoker : IBurstTrampolineInvoker4
+        {
+            private readonly Delegate method;
+
+            public TrampolineInvoker(Delegate method)
+            {
+                this.method = method;
+            }
+
+            public void Invoke(IntPtr data1Ptr, IntPtr data2Ptr, IntPtr data3Ptr, IntPtr data4Ptr)
+            {
+                ref var value1 = ref UnsafeUtility.AsRef<T1>((void*)data1Ptr);
+                ref var value2 = ref UnsafeUtility.AsRef<T2>((void*)data2Ptr);
+                ref var value3 = ref UnsafeUtility.AsRef<T3>((void*)data3Ptr);
+                ref var outValue = ref UnsafeUtility.AsRef<TOut>((void*)data4Ptr);
+                this.method(in value1, in value2, in value3, out outValue);
+            }
+        }
+    }
+
+    internal static class BurstTrampolineDispatcher
+    {
+        [AOT.MonoPInvokeCallback(typeof(BurstTrampolineInvoker0))]
+        internal static void Trampoline0(IntPtr context)
+        {
+            var handle = GCHandle.FromIntPtr(context);
+            ((IBurstTrampolineInvoker0)handle.Target!).Invoke();
+        }
+
+        [AOT.MonoPInvokeCallback(typeof(BurstTrampolineInvoker1))]
+        internal static void Trampoline1(IntPtr context, IntPtr data1Ptr)
+        {
+            var handle = GCHandle.FromIntPtr(context);
+            ((IBurstTrampolineInvoker1)handle.Target!).Invoke(data1Ptr);
+        }
+
+        [AOT.MonoPInvokeCallback(typeof(BurstTrampolineInvoker2))]
+        internal static void Trampoline2(IntPtr context, IntPtr data1Ptr, IntPtr data2Ptr)
+        {
+            var handle = GCHandle.FromIntPtr(context);
+            ((IBurstTrampolineInvoker2)handle.Target!).Invoke(data1Ptr, data2Ptr);
+        }
+
+        [AOT.MonoPInvokeCallback(typeof(BurstTrampolineInvoker3))]
+        internal static void Trampoline3(IntPtr context, IntPtr data1Ptr, IntPtr data2Ptr, IntPtr data3Ptr)
+        {
+            var handle = GCHandle.FromIntPtr(context);
+            ((IBurstTrampolineInvoker3)handle.Target!).Invoke(data1Ptr, data2Ptr, data3Ptr);
+        }
+
+        [AOT.MonoPInvokeCallback(typeof(BurstTrampolineInvoker4))]
+        internal static void Trampoline4(IntPtr context, IntPtr data1Ptr, IntPtr data2Ptr, IntPtr data3Ptr, IntPtr data4Ptr)
+        {
+            var handle = GCHandle.FromIntPtr(context);
+            ((IBurstTrampolineInvoker4)handle.Target!).Invoke(data1Ptr, data2Ptr, data3Ptr, data4Ptr);
+        }
+    }
+
+    [SuppressMessage("StyleCop.CSharp.MaintainabilityRules", "SA1402:File may only contain a single type", Justification = "Localized")]
     internal static class GarbagePrevention
     {
         [SuppressMessage("ReSharper", "CollectionNeverQueried.Local", Justification = "GC Prevention")]
