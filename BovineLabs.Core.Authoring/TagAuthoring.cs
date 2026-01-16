ce3628ae44a8b09ef4b8af246cbfd0c5a34a007d
Tim <6189147+tertle@users.noreply.github.com>
2026-01-17 09:31:43 +1100

    TagAuthoring

---
+++ BovineLabs.Core.Authoring/TagAuthoring.cs
@@ -0,0 +1,29 @@
+﻿// <copyright file="TagAuthoring.cs" company="BovineLabs">
+//     Copyright (c) BovineLabs. All rights reserved.
+// </copyright>
+
+namespace BovineLabs.Core.Authoring
+{
+    using System;
+    using Unity.Entities;
+    using UnityEngine;
+
+    /// <summary> A simple authoring script that lets you tag any entity. </summary>
+    public class TagAuthoring : MonoBehaviour
+    {
+        public ComponentAsset[] Components = Array.Empty<ComponentAsset>();
+
+        private class TagBaker : Baker<TagAuthoring>
+        {
+            public override void Bake(TagAuthoring authoring)
+            {
+                var entity = this.GetEntity(TransformUsageFlags.None);
+
+                foreach (var c in authoring.Components)
+                {
+                    this.AddComponent(entity, c.GetComponentType());
+                }
+            }
+        }
+    }
+}
