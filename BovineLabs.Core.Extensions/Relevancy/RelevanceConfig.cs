c1c9e25896800d4a1c2d34706090e75950e62159
Tim <6189147+tertle@users.noreply.github.com>
2026-02-20 07:44:08 +1100

    wip relevancy move

---
+++ BovineLabs.Core.Extensions/Relevancy/RelevanceConfig.cs
@@ -0,0 +1,16 @@
+﻿// <copyright file="RelevanceConfig.cs" company="BovineLabs">
+//     Copyright (c) BovineLabs. All rights reserved.
+// </copyright>
+
+#if !BL_DISABLE_RELEVANCY && UNITY_NETCODE
+namespace BovineLabs.Core.Relevancy
+{
+    using Unity.Entities;
+
+    public struct RelevanceConfig : IComponentData
+    {
+        public float ClampExtents; // Half size
+        public float ExpandExtents;
+    }
+}
+#endif
