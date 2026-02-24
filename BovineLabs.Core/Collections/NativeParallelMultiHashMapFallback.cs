5a91908e7d398fac1c42c64d9b0f38bab83ee08f
Tim <6189147+tertle@users.noreply.github.com>
2026-02-24 12:05:05 +1100

    Critical hash issue in NativeParallelMultiHashMapFallback

--- BovineLabs.Core/Collections/NativeParallelMultiHashMapFallback.cs
+++ BovineLabs.Core/Collections/NativeParallelMultiHashMapFallback.cs
@@ -87,7 +87,7 @@ namespace BovineLabs.Core.Collections
                     var data = this.hashMap.m_Writer.m_Buffer;
                     UnsafeUtility.WriteArrayElement(data->keys, idx, key);
                     UnsafeUtility.WriteArrayElement(data->values, idx, item);
-                    UnsafeUtility.WriteArrayElement(data->next, idx, item.GetHashCode());
+                    UnsafeUtility.WriteArrayElement(data->next, idx, key.GetHashCode());
                 }
                 else
                 {
