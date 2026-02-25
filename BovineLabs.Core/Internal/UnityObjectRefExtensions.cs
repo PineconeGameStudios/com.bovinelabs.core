4bde31e03a260f66f58fdd93fe1fc00193b1c515
Tim <6189147+tertle@users.noreply.github.com>
2026-02-26 07:39:16 +1100

    Fixed 6.3 support for Core

--- BovineLabs.Core/Internal/UnityObjectRefExtensions.cs
+++ BovineLabs.Core/Internal/UnityObjectRefExtensions.cs
@@ -12,13 +12,21 @@ namespace BovineLabs.Core.Internal
         public static EntityId GetInstanceId<T>(this UnityObjectRef<T> unityObjectRef)
             where T : Object
         {
+#if UNITY_6000_5_OR_NEWER
             return unityObjectRef.Id.entityId;
+#else
+            return unityObjectRef.Id.instanceId;
+#endif
         }
 
         public static void SetInstanceId<T>(this ref UnityObjectRef<T> unityObjectRef, EntityId entityId)
             where T : Object
         {
+#if UNITY_6000_5_OR_NEWER
             unityObjectRef.Id.entityId = entityId;
+#else
+            unityObjectRef.Id.instanceId = entityId;
+#endif
         }
     }
 }
